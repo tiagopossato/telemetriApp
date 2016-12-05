a85b1dd403bcf03a3f4854596ffef99bafc1559b
Tiago Possato <tiago.possato@yahoo.com.br>
2016-12-05 12:54:06 -0200

    gauges CSS

--- TesteWebSocekt/app.js
+++ TesteWebSocekt/app.js
@@ -11,25 +11,25 @@ var server = ws.createServer(function (conn) {
 	conn.on("text", function (str) {
 	try{
 		console.log("Received "+str)
-		setInterval(function(){
-			conn.sendText("{\"01\":"+getRandomArbitrary(0,50)+"}")			
-			},1900);
-		setInterval(function(){
-			conn.sendText("{\"02\":"+getRandomArbitrary(0,30)+"}")			
-			},1800);
-		setInterval(function(){
-			conn.sendText("{\"03\":"+getRandomArbitrary(0,50)+"}")			
-			},1700);
-		setInterval(function(){
-			conn.sendText("{\"04\":"+d+"}");
+		var i1 = setInterval(function(){
+			try{conn.sendText("{\"03\":"+getRandomArbitrary(0,50)+"}")}catch(e){clearInterval(i1);console.log(e)}
+			},100);
+		var i2 = setInterval(function(){
+			try{conn.sendText("{\"01\":"+getRandomArbitrary(0,50)+"}")}catch(e){clearInterval(i2);console.log(e)}
+			},100);
+		var i3 = setInterval(function(){
+			try{conn.sendText("{\"02\":"+getRandomArbitrary(0,30)+"}")}catch(e){clearInterval(i3);console.log(e)}			
+			},100);
+		var i4 = setInterval(function(){
+			try{conn.sendText("{\"04\":"+d+"}")}catch(e){clearInterval(i4);console.log(e)}
 			d++;
-			},1150);
-		setInterval(function(){
-			conn.sendText("{\"05\":"+getRandomArbitrary(0,35)+"}")			
-			},1750);        
-		setInterval(function(){
-			conn.sendText("{\"06\":"+getRandomArbitrary(0,43)+"}")			
-			},1650);
+			},100);
+		var i5 = setInterval(function(){
+			try{conn.sendText("{\"05\":"+getRandomArbitrary(0,35)+"}")}catch(e){clearInterval(i5);console.log(e)}
+			},100);        
+		var i6 = setInterval(function(){
+			try{conn.sendText("{\"06\":"+getRandomArbitrary(0,43)+"}")}catch(e){clearInterval(i6);console.log(e)}			
+			},100);
 	}catch(e){
 		console.log(e);
 	}
